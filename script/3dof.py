baac50a3e557c4b783ebaf4e6706f21665b1fdd8
Aie-Aie <ailengraceaspe@gmail.com>
2018-05-10 15:56:22 +0800

    Update 3dof.py

--- script/3dof.py
+++ script/3dof.py
@@ -50,6 +50,79 @@ def rotate():
 		
 
 	ang = ik.ik()
-	print(ang[1])
-	print(ang[0])
+	theta1 = ang[0]
+	theta2 = ang[1]
+
 	
+	
+	global joint1, joint2, rotz, rotz2
+	object = bge.logic.getCurrentController()
+	owner = object.owner
+
+	
+
+	#==========================constraints or limitation of the rotation
+	constjoint1 = math.radians(290)
+	constjoint2 = math.radians(310)
+
+	#==========================motion for joint 1
+	if (theta1 < 0):
+		#=====================condition for the motion
+		
+
+		if (joint1 > -constjoint1) and (rotz >= theta1) :
+			joint1 = joint1 + (math.pi/1000)*2.5
+			rotz = getmotionjoint1(theta1, joint1)
+
+
+		else:
+			owner.channels['Bone.001'].joint_rotation = mathutils.Vector([0, 0, joint1])
+			
+			
+
+			if (theta2 >= 0):
+				
+				#positive angle
+				if (joint2 < constjoint2) and (theta2 >= rotz2):
+					joint2 = joint2 - (math.pi/1000) * 2.5
+					rotz2 = getmotionjoint2(theta2, joint2)
+					#print(rotz2)	
+				#negative angle		
+			
+
+
+			elif(theta2 < 0):
+				
+				if(joint2 > -constjoint2) and (rotz2 >= theta2):
+					
+					joint2 = joint2 + math.pi/1000
+					rotz2 = getmotionjoint2(theta2, joint2)
+					#print(rotz2)	
+			
+
+	elif (theta1 >= 0):
+
+		#since counterclockwise is positive
+		if (rotz <= theta1) and (joint1 < constjoint1) :
+			joint1 = joint1 - math.pi/1000
+			rotz = getmotionjoint1(theta1, joint1)
+		
+		#motion for the joint2
+		else:
+
+			#stops the joint1
+			owner.channels['Bone.001'].joint_rotation = mathutils.Vector([0, 0, joint1])
+			if (theta2 >= 0):
+				#positive angle
+				if (joint2 < constjoint2) and (theta2 >= rotz2):
+					joint2 = joint2 - math.pi/1000
+					rotz2 = getmotionjoint2(theta2, joint2)
+				#negative angle		
+				
+
+
+			elif(theta2 < 0):
+				if(joint2 > -constjoint2) and (rotz2 >= theta2):
+					joint2 = joint2 + math.pi/1000
+					rotz2 = getmotionjoint2(theta2, joint2)
+					#print(rotz2)	
