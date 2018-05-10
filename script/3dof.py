9f283960b281ab0ee8a24e769f77a9b36d0f1e31
Aie-Aie <ailengraceaspe@gmail.com>
2018-05-10 13:40:48 +0800

    Update 3dof.py

--- script/3dof.py
+++ script/3dof.py
@@ -1,32 +1,52 @@
+
 import bge
 import math
 import mathutils
+import ik
+joint1	= 0
+joint2 	= 0
 
-x = 0
-y = 0
+
+rotz = 0
+rotz2 = 0
+
+#theta1 = float(input("Theta1: "))
+#theta2 = float(input("Theta2: "))
 def rotate():
+    
 
-	global x
-	global y
-	object	= bge.logic.getCurrentController()
-	owner	= object.owner
+	def anglediff(bone1, bone2):
+		q1, q2 =[b.pose_matrix.to_quaternion() for b in [bone1, bone2]]
+		return q1.rotation_difference(q2).to_euler()
 
-	#sensors
-	detector = object.sensors['detector']
+	def getmotionjoint1(theta1, joint1):
 
+		owner.channels['Bone.001'].joint_rotation = mathutils.Vector([0, 0, joint1])
+		owner.update()
+		
+		#========================print(dir(owner.channels['Bone.001'].rotation_quaternion))
+		
+		b1 = owner.channels['Bone.001']
+		b2 = b1.parent
+		x, y, z =anglediff(b1, b2)
+		rotz = math.degrees(z)
 
+		return rotz
 
-	#owner.channels['Bone.002'].joint_rotation	= mathutils.Vector([0, 0, 0])
-	owner.channels['Bone.001'].joint_rotation	= mathutils.Vector([0, 0, y])
-	owner.channels['Bone.002'].joint_rotation	= mathutils.Vector([0, 0, y])
-	#owner.channels['Bone.002'].joint_rotation	= mathutils.Vector([0, 0, x])
-	#owner.channels['Bone.004'].joint_rotation	= mathutils.Vector([0, 0, x])
-	#owner.channels['Bone.000'].joint_rotation=  mathutils.Vector([0, 0, x])
+	def getmotionjoint2(theta2, joint2):
 
+		owner.channels['Bone.002'].joint_rotation = mathutils.Vector([0, 0, joint2])
+		owner.update()
+		
+		#========================print(dir(owner.channels['Bone.001'].rotation_quaternion))
+		
+		b1 = owner.channels['Bone.002']
+		b2 = b1.parent
+		x, y, z =anglediff(b1, b2)
+		rotz = math.degrees(z)
 
-	owner.update()
-	x = x+ 0.01
-	y = y - 0.01
+		return rotz
 
-	if detector.positive:
-		cube.applyMovement([0, 1, 0], True)
+		
+
+	print(ik.ik())
